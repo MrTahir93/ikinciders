d1cadaf82c1c86d58fb6eeb1e1ca3b877b673465
OnOff-21072023 <tahirshammedli93@gmail.com>
2024-02-19 11:55:33 +0400

    first

---
+++ ikinciders/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ya Allah");
+
+byte var1 = 50;
+byte var2 = 40;//byte.Parse(Console.ReadLine());
+
+int sum = var1 + var2;
+
+Console.WriteLine(sum);
+
+var intmin = int.MinValue;
+var intmax = int.MaxValue;
+
+float var3 = 445.65F;
+double var4 = 451.35D;
+
+var sum2 = var3 + var4;
+
+Console.WriteLine(sum2);
+
+bool var5 = (6>5) && (4<5);
+Console.WriteLine(var5);
+
+char var6 = 'A';
+char var7 = 'B';
+
+var concat = var6.ToString() + var7.ToString();
+
+Console.WriteLine(concat);
+
+var var8 = char.IsLetter(var6);
+var var9 = char.IsNumber(var7);
+
+Console.WriteLine(var9);
+Console.WriteLine(var8);
+
+//string
+
+string var10 = "tahir oxu";
+var var11 = var10.ToUpper();
+Console.WriteLine(var11);
+
+
+
+
